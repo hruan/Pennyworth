4f357278dade539d2ff5379e109a73db5780e536
Hang Ruan <h@ruan.se>
2012-07-13 18:28:16 +0200

    Documentation for DropHelper

--- Pennyworth/DropHelper.cs
+++ Pennyworth/DropHelper.cs
@@ -4,28 +4,37 @@ using System.IO;
 using System.Linq;
 
 namespace Pennyworth {
-    public static class DropHelper {
-        public static IEnumerable<String> GetAssembliesFromDropData(IEnumerable<FileInfo> data) {
-            Func<FileInfo, Boolean> isDir  = fi => ((fi.Attributes & FileAttributes.Directory) == FileAttributes.Directory);
-            Func<FileInfo, Boolean> isFile = fi => ((fi.Attributes & FileAttributes.Directory) != FileAttributes.Directory);
+	public static class DropHelper {
+		/// <summary>
+		/// Find assemblies in the files and/or directories
+		/// </summary>
+		/// <param name="data">paths to files to sift through</param>
+		/// <returns>absolute paths to assemblies found in <paramref name="data"/></returns>
+		public static IEnumerable<String> GetAssembliesFromDropData(IEnumerable<FileInfo> data) {
+			Func<FileInfo, Boolean> isDir  = fi => ((fi.Attributes & FileAttributes.Directory) == FileAttributes.Directory);
+			Func<FileInfo, Boolean> isFile = fi => ((fi.Attributes & FileAttributes.Directory) != FileAttributes.Directory);
 
-            var files = data.Where(isFile);
-            var dirs  = data.Where(isDir);
+			var files = data.Where(isFile);
+			var dirs  = data.Where(isDir);
 
-            var assembliesInDirs =
-                dirs.SelectMany(dir => Directory.EnumerateFiles(dir.FullName, "*", SearchOption.AllDirectories));
+			var assembliesInDirs =
+				dirs.SelectMany(dir => Directory.EnumerateFiles(dir.FullName, "*", SearchOption.AllDirectories));
 
-            return files.Select(fi => fi.FullName)
-                .Concat(assembliesInDirs)
-                .Where(path => (path.EndsWith(".exe", StringComparison.OrdinalIgnoreCase)
-                                || path.EndsWith(".dll", StringComparison.OrdinalIgnoreCase))
-                               && !path.Contains("vshost"));
-        }
+			return files.Select(fi => fi.FullName)
+				.Concat(assembliesInDirs)
+				.Where(path => (path.EndsWith(".exe", StringComparison.OrdinalIgnoreCase)
+								|| path.EndsWith(".dll", StringComparison.OrdinalIgnoreCase))
+							   && !path.Contains("vshost"));
+		}
 
-        public static String GetBaseDir(IEnumerable<FileInfo> data) {
-            if (data == null || !data.Any()) return String.Empty;
+		/// <summary>
+		/// Find the absolute path of the base directory
+		/// </summary>
+		/// <returns>absolute path to base directory</returns>
+		public static String GetBaseDir(IEnumerable<FileInfo> data) {
+			if (data == null || !data.Any()) return String.Empty;
 
-            return data.First().FullName;
-        }
-    }
+			return data.First().FullName;
+		}
+	}
 }
