0dcbc7106cf6b85c4bc67efd828e53b2f1da1250
Hang Ruan <h@ruan.se>
2012-08-31 12:14:56 +0200

    Added test for public fields

--- Pennyworth.Tests/InspectionHelperTests.cs
+++ Pennyworth.Tests/InspectionHelperTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using NUnit.Framework;
 using Pennyworth.Inspection;
 using Ploeh.AutoFixture;
@@ -11,15 +9,14 @@ namespace Pennyworth.Tests {
 	[TestFixture]
 	public class InspectionHelperTests {
 		private readonly IFixture _fixture = new Fixture().Customize(new MultipleCustomization());
-        private readonly Assembly _stub = StubAssemblyBuilder.Create(new Fixture().CreateAnonymous<String>());
+		private readonly Assembly _stub = StubAssemblyBuilder.Create(new Fixture().CreateAnonymous<String>());
 
 		private InspectionHelper _sut;
 
 		[SetUp]
 		public void Setup()
 		{
-			_fixture.Register<InspectionHelper>(() =>
-				new InspectionHelper(_stub));
+			_fixture.Register<InspectionHelper>(() => new InspectionHelper(_stub));
 			_sut = _fixture.CreateAnonymous<InspectionHelper>();
 		}
 
@@ -29,10 +26,16 @@ namespace Pennyworth.Tests {
 			Assert.AreEqual(1, _sut.GetRecursiveCalls().Count());
 		}
 
-        [Test]
-        public void GetIndirectCalls_ReturnsTwoFaults()
-        {
-            Assert.AreEqual(2, _sut.GetIndirectRecursiveCalls().Count());
-        }
+		[Test]
+		public void GetIndirectCalls_ReturnsTwoFaults()
+		{
+			Assert.AreEqual(2, _sut.GetIndirectRecursiveCalls().Count());
+		}
+
+		[Test]
+		public void GetPublicFields_ReturnsOneFault()
+		{
+			Assert.AreEqual(1, _sut.GetPublicFields().Count());
+		}
 	}
 }
--- Pennyworth.Tests/StubAssemblyBuilder.cs
+++ Pennyworth.Tests/StubAssemblyBuilder.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Reflection;
 using System.Reflection.Emit;
 
@@ -8,19 +7,22 @@ namespace Pennyworth.Tests {
 		public static Assembly Create(String name)
 		{
 			var ab = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(name), AssemblyBuilderAccess.RunAndSave, AppDomain.CurrentDomain.BaseDirectory);
-			var mb = ab.DefineDynamicModule(name, name + ".dll");
+			var mb = ab.DefineDynamicModule(name);
 
+			// Create type with public fields
 			var pubFieldsType = mb.DefineType(name + "_PublicField", TypeAttributes.Class | TypeAttributes.Public);
 			pubFieldsType.DefineField("ThisIsAPublicField", typeof(Int32), FieldAttributes.Public);
-            pubFieldsType.CreateType();
+			pubFieldsType.CreateType();
 
+			// Create type with a recursive method
 			var recMethodType = mb.DefineType(name + "_RecursiveMethod", TypeAttributes.Class | TypeAttributes.Public);
 			var recMethodBuilder = recMethodType.DefineMethod("ThisIsARecursiveMethod", MethodAttributes.Public);
 			var recMethodIl = recMethodBuilder.GetILGenerator();
 			recMethodIl.Emit(OpCodes.Callvirt, recMethodBuilder);
 			recMethodIl.Emit(OpCodes.Ret);
-            recMethodType.CreateType();
-			
+			recMethodType.CreateType();
+
+			// Create type with two methods which call one another
 			var inRecurType = mb.DefineType(name + "_IndirectRecursiveMethod", TypeAttributes.Class | TypeAttributes.Public);
 			var inRecur1 = inRecurType.DefineMethod("IndirectRecursiveA", MethodAttributes.Public);
 			var inRecur2 = inRecurType.DefineMethod("IndirectRecursiveB", MethodAttributes.Public);
@@ -32,9 +34,9 @@ namespace Pennyworth.Tests {
 			var inRecurIl2 = inRecur2.GetILGenerator();
 			inRecurIl2.Emit(OpCodes.Callvirt, inRecur1);
 			inRecurIl2.Emit(OpCodes.Ret);
-            inRecurType.CreateType();
+			inRecurType.CreateType();
 
-            return ab;
+			return ab;
 		}
 	}
 }
